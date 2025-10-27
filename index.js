488f282f5ada4818f8bbaa7928289ce978241edd
LindaPermata <permatalinda531@gmail.com>
2025-10-27 09:18:39 +0700

    inisialisasi

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-'use strict';
+const express = require('express');
+const app = express();
+const PORT= 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: false 
+}));
+
+
